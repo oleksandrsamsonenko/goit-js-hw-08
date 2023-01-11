c3a007ee3cb90d024bb5e2bb73567c8fba78ea4a
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-11 18:17:08 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,22 +2,22 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryMarkup = document.querySelector(`.gallery`);
+// const galleryMarkup = document.querySelector(`.gallery`);
 
-galleryItems.forEach(element => {
-  galleryMarkup.insertAdjacentHTML(
-    'beforeend',
-    `<a class="gallery__item" href="${element.original}">
-  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
-</a>`
-  );
-});
+// galleryItems.forEach(element => {
+//   galleryMarkup.insertAdjacentHTML(
+//     'beforeend',
+//     `<a class="gallery__item" href="${element.original}">
+//   <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+// </a>`
+//   );
+// });
 
-let gallery = new SimpleLightbox('.gallery a', {
-  captionsData: `alt`,
-  captionDelay: 250,
-  showCounter: false,
-  disableRightClick: true,
-});
+// let gallery = new SimpleLightbox('.gallery a', {
+//   captionsData: `alt`,
+//   captionDelay: 250,
+//   showCounter: false,
+//   disableRightClick: true,
+// });
 
-gallery.on('show.simplelightbox', function () {});
+// gallery.on('show.simplelightbox', function () {});
