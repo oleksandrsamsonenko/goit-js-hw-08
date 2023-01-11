97f505b8bd6447a13d991ea3d40909bb89d28b40
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-11 18:01:39 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryMarkup = document.querySelector(`.gallery`);
+
+galleryItems.forEach(element => {
+  galleryMarkup.insertAdjacentHTML(
+    'beforeend',
+    `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`
+  );
+});
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+  showCounter: false,
+  disableRightClick: true,
+});
+
+gallery.on('show.simplelightbox', function () {});
