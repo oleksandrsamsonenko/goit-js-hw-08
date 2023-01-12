9fc8168362eb65ca5566e0ecbeb04bd3ee103ebc
Oleksandr Samsonenko <invizibji@gmail.com>
2023-01-12 16:10:06 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        console.log(`throttle current time:`, seconds);
+        localStorage.setItem('currentTime', seconds);
+      })
+      .catch(function (error) {});
+  }, 1000)
+);
+
+console.log('saved time ->', localStorage.getItem(`currentTime`));
+const currentTime = localStorage.getItem(`currentTime`);
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
